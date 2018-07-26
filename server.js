8c2d95d4e23e29a1ab341acd0e05b86ed67d3ae1
ajomadlabs <ajojohn555@gmail.com>
2018-07-26 10:51:57 +0530

    add support for es6 syntax and create server

---
+++ server.js
@@ -0,0 +1,20 @@
+// Importing required modules
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+// Defining port
+const port = process.env.PORT || 9000;
+
+// Defining app
+const app = express();
+
+// Defining middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Listening to port
+app.listen(port);
+console.log('Server running on:' + ' ' + 'http://localhost:' + port);
